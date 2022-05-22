1cb56286b839afdafe997867abfde085304c4f9b
markov76 <101133846+markov76@users.noreply.github.com>
2022-05-23 00:42:20 +0300

    lisätty fetch

--- js/js.js
+++ js/js.js
@@ -27,12 +27,23 @@ f.classList.add("important");
 
 let button = document.getElementById("id");
 
-  button.addEventListener("click", doSome)
+//  button.addEventListener("click", fetch )
 
-  function doSome() {
+/*  function doSome() {
     fetch('https://dog.ceo/api/breeds/image/random')
 .then(response => response.json())
 .then(data => document.getElementById('print-here').innerHTML =
-JSON.stringify(data.images));
-  }
-    
+JSON.stringify(data.message));
+  }*/
+let btn = document.getElementById("btnClick");
+let image = document.getElementById("image");
+
+btn.addEventListener("click", function () {
+  fetch("https:dog.ceo/api/breeds/image/random")
+  .then(res => res.json())
+  .then(result => {
+    console.log(result)
+    image.src = result.message
+  })
+  .catch(err=>console.log(err))
+})
