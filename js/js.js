15821805565adcfd51bc27e3fe701b5279cc9204
markov76 <101133846+markov76@users.noreply.github.com>
2022-06-01 22:40:48 +0300

    ui suunnitelma päivitetty uudestaan

--- js/js.js
+++ js/js.js
@@ -25,15 +25,25 @@ let f = document.getElementById("footter");
 
 f.classList.add("important");
 
-let btn = document.getElementById("btnClick");
-let image = document.getElementById("image");
+//let button = document.getElementById("id");
 
-btn.addEventListener("click", function () {
-  fetch("https:dog.ceo/api/breeds/image/random")
-  .then(res => res.json())
-  .then(result => {
-    console.log(result)
-    image.src = result.message
+ // button.addEventListener("click", fetch )
+
+/*  function doSome() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+.then(response => response.json())
+.then(data => document.getElementById('print-here').innerHTML =
+JSON.stringify(data.message));
+  }*/
+  let btn = document.getElementById("btnClick");
+  let image = document.getElementById("image");
+  
+  btn.addEventListener("click", function () {
+    fetch("https:dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(result => {
+      console.log(result)
+      image.src = result.message
+    })
+    .catch(err=>console.log(err))
   })
-  .catch(err=>console.log(err))
-})
